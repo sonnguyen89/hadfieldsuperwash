7b2c50b220d461b59b92a6e85ac99bdcc8b8c85b
sonnguyen89 <nam_son14@yahoo.com>
2018-07-30 19:10:08 +1000

    update price page

--- wp-content/themes/illdy/page-templates/template-price.php
+++ wp-content/themes/illdy/page-templates/template-price.php
@@ -13,7 +13,8 @@
     <div class="row">
         <div class="col-sm-12">
             <div class="page-header">
-                <h3>Price List</h3>
+                <h3>Laundry Service Price List</h3>
+                <p>(Customer Drop and Go)</p>
             </div>
             <table class="table">
                 <thead>
@@ -22,7 +23,8 @@
                     <th>Wash</th>
                     <th>Deter</th>
                     <th>Dry</th>
-                    <th>Fold</th>
+                    <th>Bag</th>
+                    <th>Total</th>
                 </tr>
                 </thead>
 
@@ -32,29 +34,66 @@
                     <td>$5</td>
                     <td>$2</td>
                     <td>$4</td>
-                    <td>$15</td>
+                    <td>$1</td>
+                    <td>$12</td>
                 </tr>
                 <tr>
                     <th scope="row">12 kg</th>
                     <td>$7</td>
                     <td>$2</td>
-                    <td>$4</td>
-                    <td>$17</td>
+                    <td>$5</td>
+                    <td>$1</td>
+                    <td>$15</td>
                 </tr>
-                <tr class="active">
+                <tr class="info">
                     <th scope="row">14 kg</th>
                     <td>$9</td>
                     <td>$2</td>
-                    <td>$5</td>
-                    <td>$20</td>
+                    <td>$6</td>
+                    <td>$1</td>
+                    <td>$18</td>
                 </tr>
                 </tbody>
             </table>
             <p>
-               Note:  Free Labour service. If you supply your own laundry  detergent, we will give you discounted price
+               Note: Free labour service. If you supply your own laundry  detergent, we will give you discounted price
             </p>
         </div><!--/.col-sm-12-->
     </div><!--/.row-->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-header">
+                <h3>Dry Cleaner Services Price List</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-action list-group-item-info">Doona Wash  $25</li>
+                <li class="list-group-item">** 4 Garment $20</li>
+                <li class="list-group-item list-group-item-action list-group-item-info">Shirt Press only $3.00 each</li>
+            </ul>
+            <p>
+                **exclusion applies.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-header">
+                <h3>Detergent Price List</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-action list-group-item-info">20 Litter for $50</li>
+                <li class="list-group-item">Fill up $3.00 per litter</li>
+            </ul>
+        </div>
+    </div>
     <div class="row">
         <div class="col-sm-12">
             <div class="doona-wash-section" style="text-align: center ;margin: 45px 0;">
